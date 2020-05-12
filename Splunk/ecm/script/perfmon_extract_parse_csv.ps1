945725d6ff3a902ea9557a5f218a037621b508d2
Ranti Familusi <ranti.familusi@nttdata.com>
2020-05-12 15:46:18 -0600

    refactored to handle multiple csv in file

--- Splunk/ecm/script/perfmon_extract_parse_csv.ps1
+++ Splunk/ecm/script/perfmon_extract_parse_csv.ps1
@@ -1,17 +1,28 @@
-Set-ExecutionPolicy Unrestricted
+Set-ExecutionPolicy Unrestricted -Scope CurrentUser
 
+$ourpath = Get-Location
+$folderPath = $ourpath.Path+"\csv\"
+$folderPath
+$files = Get-ChildItem -Path $folderPath
+foreach  ($file in $files.Name)
+{
+   $conf_file_name = $file  -replace ".csv$" , ".conf"
 
-$csv_path = $ourpath.Path+"\csv\windows_performance_counters.csv"
+$ourpath.Path
+$csv_path = $ourpath.Path+"\csv\"+$file
 $csv_path
 $performance_objects = Import-Csv $csv_path
-$performance_objects
 foreach ($item in $performance_objects) {
-   $conf += "[perfmon://" + $item.Object + "]" + ([Environment]::NewLine) + "object = " + $item.Object + ([Environment]::NewLine) + "counters = " + $item.Counters + ([Environment]::NewLine) + "instances = " + $item.Instance + ([Environment]::NewLine) + "disabled = 0" + ([Environment]::NewLine) + "interval = 60" + ([Environment]::NewLine) + ([Environment]::NewLine)
+   $name = $item.Object -replace "�", " "
+   
+   $conf += "[perfmon://" + $name + ([Environment]::NewLine) + "object = " + $item.Object + ([Environment]::NewLine) + "counters = " + $item.Counters + ([Environment]::NewLine) + "instances = " + $item.Instance + ([Environment]::NewLine) + "disabled = 0" + ([Environment]::NewLine) + "interval = 60" + ([Environment]::NewLine) + ([Environment]::NewLine)
 }
 
-$filePath = $ourpath.Path + "\ps.input.conf"
+$filePath = $ourpath.Path + "\"+ $conf_file_name
 New-Item -Path $filePath -ItemType File -Force
 
 Set-Content -Path $filePath -Value $conf
+$conf = ""
+}
 
 
